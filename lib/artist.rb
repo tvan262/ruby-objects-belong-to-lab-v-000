376494b9c248afafeb7084003b46bf93edf3997c
Tuong Van Nguyen <tvan262@yahoo.com>
2018-06-02 02:40:48 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,14 @@
+class Artist
+  attr_accessor :name, :genre
+ 
+  def initialize(name, genre) 
+    @name = name
+    @genre = genre
+  end
+ 
+end
+ 
+drake = Artist.new("Drake", "rap")
+hotline_bling = Song.new("Hotline Bling")
+ 
+hotline_bling.artist = drake
